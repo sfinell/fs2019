43f42845961c2fef6e5714fb3070e50175eb0f56
Sam Finell <sfinell@gmail.com>
2019-06-12 23:22:45 +0300

    osa 1.14: anekdootit, step3, almost clean

--- osa1/anekdootit/src/index.js
+++ osa1/anekdootit/src/index.js
@@ -5,56 +5,23 @@ const getNextAnecdote = (count, prev) => {
   let result = prev
   while (result === prev) {
     result = Math.floor(Math.random() * count)
-    console.log("randomize...[ 0,", count, "[:", result)
+//    console.log("randomize...[ 0,", count, "[:", result)
   }
   return result
-}
-
-// const getNextAnecdoteText = (anecdotes, selected) => {
-//   if (selected < 0) {
-//     console.log("first time:", selected)
-//     selected = getNextAnecdote(anecdotes)
-//   }
-//   console.log("now index:", selected)
-//   return anecdotes[selected]
-// }
-
-// const MyState = (props) => {
-//   console.log("MyState - count:", props.count)
-//   return (
-//     <p> MyState: {props.count} </p>
-//   )
-// }
-
-const Votes = (props) => {
-  console.log("Votes - count:", props.count)
-  return (
-    <p>
-      AllVotes: {props.votes} <br/>
-      Vote[0]: {props.votes[0]} <br/>
-      Vote[1]: {props.votes[1]} <br/>
-      Vote[2]: {props.votes[2]} <br/>
-      Vote[3]: {props.votes[3]} <br/>
-      Vote[4]: {props.votes[4]} <br/>
-      Vote[5]: {props.votes[5]} <br/>
-    </p>
-  )
 }
 
 const Anecdote = (props) => {
   return (
     <div>
       <h1>{props.header}</h1>
-      selected: {props.selected} / {props.anecdotes.length} <br/>
-      <Votes count={6} votes={props.votes} />
-      {props.selected}: {props.anecdotes[props.selected]} <br/>
+      {props.anecdotes[props.selected]} <br/>
       has {props.votes[props.selected]} votes
     </div>
   )
 }
 
 const createVotes = (count) => {
-  console.log("createVotes():", count)
+//  console.log("createVotes():", count)
   return Array.apply(null, new Array(count)).map(Number.prototype.valueOf,0)
 }
 
@@ -66,7 +33,7 @@ const getMostVoted = (votes) => {
       mostVoted = i;
     }
   }
-  console.log("getMostVoted():", mostVoted)
+//  console.log("getMostVoted():", mostVoted)
   return mostVoted
 }
 
@@ -74,37 +41,34 @@ const App = (props) => {
   const [selected, setSelected] = useState(-1)
   const [votes, setVotes] = useState()
   const count = props.anecdotes.length
-  console.log("A: ", selected)
-  console.log("count:", count, "votes: ", votes)
+//  console.log("A: ", selected)
+//  console.log("count:", count, "votes: ", votes)
 
   if (selected < 0) {
     setSelected(getNextAnecdote(count, selected))
     setVotes(createVotes(props.anecdotes.length))
-    console.log("B: ", selected)
+//    console.log("B: ", selected)
     return
   }
-  console.log("C: ", selected)
+//  console.log("C: ", selected)
 
   const handleNextAnecdote = () => {
-    const prev = selected
     const next = getNextAnecdote(count, selected)
-///    setSelected(getNextAnecdote(props.anecdotes))
+//    console.log("prev->next: ", selected, "->", next)
     setSelected(next)
-    console.log("prev->next: ", prev, "->", next)
   }
 
   const handleVoteAnecdote = () => {
     const copy = [...votes]
     copy[selected] += 1
     setVotes(copy)
-    console.log("voted: ", selected, ":", copy[selected])
+//    console.log("voted: ", selected, ":", copy[selected])
   }
 
   const mostVoted = getMostVoted(votes)
 
   return (
     <div>
-        <h1>Anecdote of the day</h1>
         <Anecdote
           header="Anecdote of the day"
           anecdotes={props.anecdotes}
