d14849d7a558cc498c2b1a9fbcd89ce41bc37795
Sam Finell <sfinell@gmail.com>
2019-06-22 15:27:45 +0300

    osa 2.8: puhelinluettelo step3

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,33 +2,39 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' },
-    { name: 'Ada Lovelace' }
+    { name: 'Arto Hellas', number: '555-123' },
+    { name: 'Ada Lovelace', number: '555-124' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
     console.log('button clicked', event.target)
-//    console.log('event:', event)
-    const nameObject = {
-      name: newName
-    }
+  // console.log('event:', event)
   // console.log('persons:', persons)
-  // console.log('nameObject:', nameObject)
-  // console.log('findIndex:', persons.findIndex(e => e.name === newName))
-  if (persons.findIndex(e => e.name === newName) >= 0) {
+    if (persons.findIndex(e => e.name === newName) >= 0) {
       alert(`${newName} is already added to phonebook`)
       return
+    }
+    const nameObject = {
+      name: newName,
+      number: newNumber
     }
     // console.log("uus:", persons.concat(nameObject))
     setPersons(persons.concat(nameObject))
     setNewName("")
+    setNewNumber("")
   }
 
   const handleNameChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
   }
 
   return (
@@ -41,11 +47,17 @@ const App = () => {
             onChange={handleNameChange}/>
         </div>
         <div>
+          number: <input
+            value={newNumber}
+            onChange={handleNumberChange}/>
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => <div key={person.name}>{person.name} </div>)}
+      {persons.map(person =>
+        <div key={person.name}>{person.name} {person.number} </div>)}
       <div>debug: {newName}</div>
     </div>
   )
