39d67fbcf817d289c0881b1eac41a48dbd6bcc0f
Sam Finell <sfinell@gmail.com>
2019-06-23 18:48:18 +0300

    osa 2.11: puhelinluettelo step6

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = (props) => {
   return (
@@ -52,6 +53,19 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+  useEffect(() =>
+  {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response =>
+        {
+          console.log('response:', response)
+          setPersons(response.data)
+        })
+  }, [])
+  console.log('render', persons.length, 'persons')
 
   const addName = (event) => {
     event.preventDefault()
