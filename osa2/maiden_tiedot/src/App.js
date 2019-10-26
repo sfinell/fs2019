a59190c88ea562bd20a1484c657052d1cdc98ad8
Sam Finell <sfinell@gmail.com>
2019-10-26 11:20:01 +0300

    osa 2.14: maiden tiedot, step3

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -4,6 +4,9 @@ import axios from 'axios'
 // import logo from './logo.svg';
 // import './App.css';
 
+// api.openweathermap.org/data/2.5/weather?q={city name},{country code}
+// apikey: 8f59a64a1a68ca5eb989cb69681d431b
+
 const Filter = (props) => {
   return (
     <div>find countries <input
@@ -48,6 +51,44 @@ const OneCountry = ({country}) => {
   )
 }
 
+const Weather = ({country}) => {
+  const [ weather, setWeather ] = useState([])
+  console.log("piirretään...")
+  console.log('alku-{country.capital}-'+country.capital+','+country.alpha2Code)
+  const url = 'https://api.openweathermap.org/data/2.5/weather?q='+country.capital+','+country.alpha2Code+'&appid=8f59a64a1a68ca5eb989cb69681d431b'
+  if (weather.length === 0) {
+    console.log("katotanpa...")
+    axios
+      .get(url)
+      .then(response =>
+        {
+          console.log('vastaus:', response)
+          setWeather(response.data)
+        })
+    return(<div>Loading weather...</div>)
+  }
+  console.log("sää:", weather)
+
+  const nimi = weather.name
+  const lampotila = (weather.main.temp - 273.15).toFixed(1)
+  const tuuli = weather.wind
+  const suunta = (tuuli.hasOwnProperty("deg")) ? "direction " + tuuli.deg + "&deg;" : ""
+  const iconcode = weather.weather[0].icon
+  const iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+  console.log("nimi:", nimi)
+  console.log("tuuli:", tuuli)
+  console.log("suunta:", suunta)
+  console.log("iconcode:", iconcode)
+  console.log("iconurl:", iconurl)
+  return (
+    <div><h2>Weather in {nimi}</h2>
+    <b>temperature:</b> {lampotila} &#8451;<br/>
+    <img id="wicon" src={iconurl} alt="Weather icon"/><br/>
+    <b>wind:</b> {tuuli.speed} m/s direction {tuuli.deg}&deg;
+    </div>
+  )
+}
+
 const Countries = (props) => {
   console.log("countries:", props.countries.length)
   const count = props.countries.length
@@ -61,7 +102,10 @@ const Countries = (props) => {
   }
   else if (count > 0) {
     return (
+      <div>
       <OneCountry country={props.countries[0]} />
+      <Weather country={props.countries[0]} />
+      </div>
     )
   }
   return (
@@ -72,6 +116,7 @@ const Countries = (props) => {
 const App = () => {
   const [ countries, setCountries] = useState([])
   const [ filter, setFilter ] = useState('swi')
+  const [ capital, setCapital ] = useState('')
 
   useEffect(() =>
   {
@@ -82,6 +127,9 @@ const App = () => {
         {
           console.log('response:', response)
           setCountries(response.data)
+          if (response.data.length === 1) {
+            setCapital(response.data[0].capital)
+          }
         })
   }, [])
 
@@ -91,6 +139,24 @@ const App = () => {
 
   const re = new RegExp(filter, "i");
   const filteredCountries = () => countries.filter(e => re.test(e.name))
+
+  // useEffect(() =>
+  // {
+  //   console.log('effect for weather')
+  //   console.log('filteredCountries.length', filteredCountries.length)
+  //   if (filteredCountries().length != 1) {
+  //     console.log("...not yet")
+  //     return
+  //   }
+  //   axios
+  //     .get('https://api.openweathermap.org/data/2.5/weather?q={city name},{country code}')
+  //     .then(response =>
+  //       {
+  //         console.log('response:', response)
+  //         setWeather(response.data)
+  //       })
+  // }, [filter])
+
 
   return (
     <div>
